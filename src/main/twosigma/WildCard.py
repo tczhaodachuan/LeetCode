2bf8f92ee4ff46229c208d07f1e2a0a832601ee6
tczhaodachuan <tczhaodachuan@gmail.com>
2018-11-18 12:23:11 -0500

    leetcode
    Signed-off-by: tczhaodachuan <tczhaodachuan@gmail.com>

--- src/main/twosigma/WildCard.py
+++ src/main/twosigma/WildCard.py
@@ -11,30 +11,33 @@ def isMatching(s, p):
     n = len(p)
 
     i = j = 0
-    workdMatchStarPosition = lastStarPosition = -1
+    wordMatchStarPosition = lastStarPosition = -1
 
     while i < m:
         if j < n and (s[i] == p[j] or p[j] == '?'):
             i += 1
             j += 1
         elif j < n and p[j] == '*':
-            workdMatchStarPosition = i
+            wordMatchStarPosition = i
             lastStarPosition = j
             j += 1
         elif lastStarPosition != -1:
-            # pattern moves back to star + 1
+            # When there is a star in the pattern also matches the the string so far
+            # pattern moves back to star, likes nothing happened
             j = lastStarPosition
             j += 1
-            # word moves back to previous position  + 1
-            workdMatchStarPosition += 1
-            i = workdMatchStarPosition
+            # i moves forward one to continue the search
+            wordMatchStarPosition += 1
+            i = wordMatchStarPosition
         else:
             return False
 
-    # at the end
+    # we scan through the string s, nothing matches after the last star position
+    # so the only possible is double *
     # if s = abcde, p = ab*k
     # when it exits, j is still at position *
     # so if the only option to match is the rest of the pattern has to be *, cannot even be ?
+    print j, n
     while j < n:
         if p[j] != '*':
             return False
@@ -45,3 +48,5 @@ def isMatching(s, p):
 
 if __name__ == '__main__':
     print isMatching('wheat', "*")
+    print isMatching('abcde', "?b*cde")
+    print isMatching('abcde', "?b*cda")

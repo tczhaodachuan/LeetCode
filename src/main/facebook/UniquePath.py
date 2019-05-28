02a4886e56d7093aa0fc2c7d4636be1f1114ec52
tczhaodachuan <tczhaodachuan@gmail.com>
2019-05-27 23:11:12 -0400

    Leet codes
    Signed-off-by: tczhaodachuan <tczhaodachuan@gmail.com>

--- src/main/facebook/UniquePath.py
+++ src/main/facebook/UniquePath.py
@@ -74,6 +74,24 @@ class Solution(object):
                     dp[i][j] = 0
         return dp[m - 1][n - 1]
 
+    def minPathSum(self, grid):
+        m = len(grid)
+        n = len(grid[0])
+
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+        # dp[i][j] stands for to position i, j the min path sum
+        dp[0][0] = grid[0][0]
+
+        for i in range(1, m):
+            dp[i][0] = dp[i - 1][0] + grid[i][0]
+        for j in range(1, n):
+            dp[0][j] = dp[0][j - 1] + grid[0][j]
+
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = min((dp[i - 1][j] + grid[i][j]), (dp[i][j - 1] + grid[i][j]))
+        return dp[m - 1][n - 1]
+
 
 if __name__ == '__main__':
     s = Solution()
@@ -88,3 +106,9 @@ if __name__ == '__main__':
     print s.uniquePathsTwo([[0], [0]])
     print s.uniquePathsTwo([[0, 0], [1, 0]])
     print s.uniquePathsTwo([[0, 0, 0], [0, 1, 0], [0, 0, 0]])
+
+    print s.minPathSum([
+        [1, 3, 1],
+        [1, 5, 1],
+        [4, 2, 1]
+    ])
